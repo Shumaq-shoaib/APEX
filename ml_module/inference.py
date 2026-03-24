7203603220aa023decfe93eeedc0a23919c9a565
Fatima Munir <fatimamunir20032003@gmail.com>
2026-03-25 02:30:55 +0500

    Refactor inference.py by removing TextCombiner
    Removed the extract_text_field function and TextCombiner class, and imported TextCombiner from transformers.

--- ml_module/inference.py
+++ ml_module/inference.py
@@ -3,20 +3,7 @@ import pandas as pd
 import re
 import os
 from sklearn.base import BaseEstimator, TransformerMixin
-
-# ---- TextCombiner ----
-def extract_text_field(df):
-    return (
-        df['request.url'].fillna('').astype(str) + ' ' +
-        df['request.body'].fillna('').astype(str) + ' ' +
-        df['request.headers.Cookie'].fillna('').astype(str) + ' ' +
-        df['request.headers.User-Agent'].fillna('').astype(str)
-    )
-
-class TextCombiner(BaseEstimator, TransformerMixin):
-    def fit(self, X, y=None): return self
-    def transform(self, X): return extract_text_field(X)
-
+from .transformers import TextCombiner 
 # Path to your saved pipeline
 MODEL_PATH = os.path.join(os.path.dirname(__file__), 'model', 'api_security_pipeline.pkl')
 
